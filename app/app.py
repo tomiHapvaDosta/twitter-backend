5fd0577c84bc9d50d71a10c0fd24f66047275819
Tomislav Simeonov <filipsim1@abv.bg>
2026-02-14 17:52:43 +0200

    Compulsory setup for fastapi

---
+++ app/app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, HTTPException, Form, Depends, status
+from app.db import Post, create_db_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
+from sqlalchemy import select
+from uuid import UUID, uuid4
+from app.schemas import PostCreate, PostResponse, DeleteRequest, UserRead, UserCreate, UserUpdate
+from app.users import auth_backend, current_active_user, fastapi_users, User
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(fastapi_users.get_auth_router(auth_backend), prefix='/auth/jwt', tags=['auth'])
+app.include_router(fastapi_users.get_register_router(UserRead, UserCreate), prefix='/auth', tags=['auth'])
+app.include_router(fastapi_users.get_reset_password_router(), prefix='/auth', tags=['auth'])
+app.include_router(fastapi_users.get_verify_router(UserRead), prefix='/auth', tags=['auth'])
+app.include_router(fastapi_users.get_users_router(UserRead, UserUpdate), prefix='/auth', tags=['users'])
